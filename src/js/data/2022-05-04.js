73b5196a151325fe5053e8f6c3ccfdadb2a1178d
1qqru <108229625+1qqru@users.noreply.github.com>
2022-06-26 12:21:00 -0300

    Add files via upload

---
+++ src/js/data/2022-05-04.js
@@ -0,0 +1,1161 @@
+dataSetVersion = "2022-05-04";
+dataSet[dataSetVersion] = {};
+
+dataSet[dataSetVersion].options = [
+  {
+    name: "Filter by Group",
+    key: "group",
+    tooltip: "Check this to restrict to certain groups.",
+    checked: false,
+    sub: [
+      { name: "AOA", key: "AOA" },
+      { name: "Red Velvet", key: "redvelvet" },
+      { name: "GFriend", key: "gfriend" },
+      { name: "Oh My Girl", key: "omg" },
+      { name: "Twice", key: "twice" },
+      { name: "Blackpink", key: "blackpink" },
+      { name: "MOMOLAND", key: "momoland" },
+      { name: "Dreamcatcher", key: "dc" },
+      { name: "Iz*One", key: "izone" },
+      { name: "(G)I-dle	", key: "gidle" },
+      { name: "Itzy", key: "itzy" },
+      { name: "Aespa", key: "aespa" },
+      { name: "STAYC", key: "stayc" },
+      { name: "Everglow", key: "everglow" },
+      { name: "Fromis_9", key: "fromis9" },
+      { name: "Loona", key: "loona" },
+      { name: "Purple Kiss", key: "purplekiss" },
+      { name: "Weeekly", key: "weeekly" },
+      { name: "Ive", key: "ive"},
+      { name: "Woo!ah!", key: "wooah" },
+      { name: "LESSERAFIM", key: "lesserafim" },
+    ]
+  },
+  {
+    name: "Filter by Generation",
+    key: "gen",
+    tooltip: "Check this to restrict to groups that debuted in certain generations.",
+    checked: false,
+    sub: [ 
+      { name: "1st Gen", key: "gen1" },
+      { name: "2nd Gen", key: "gen2" }, 
+      { name: "3rd Gen", key: "gen3" }, 
+      { name: "4th Gen", key: "gen4" },
+    ]
+  },
+];
+
+dataSet[dataSetVersion].characterData = [
+  {
+    name: "Hyebin (Momoland)",
+    img: "uuBhSVr.png",
+    opts: {
+      group: [ "momoland" ],
+      gen: ["gen3"]  
+      }
+  },
+  {
+    name: "Jane (Momoland)",
+    img: "l2q3C1S.png",
+    opts: {
+      group: [ "momoland" ],
+      gen: ["gen3"]  
+      }
+  },
+  {
+    name: "Nayun (Momoland)",
+    img: "iF2Y4XP.png",
+    opts: {
+      group: [ "momoland" ],
+      gen: ["gen3"]  
+      }
+  },
+  {
+    name: "JooE (Momoland)",
+    img: "SlNbq3p.png",
+    opts: {
+      group: [ "momoland" ],
+      gen: ["gen3"]  
+      }
+  },
+  {
+    name: "Ahin (Momoland)",
+    img: "YFbArYk.png",
+    opts: {
+      group: [ "momoland" ],
+      gen: ["gen3"]  
+      }
+  },
+  {
+    name: "Nancy (Momoland)",
+    img: "UFYhhFQ.png",
+    opts: {
+      group: [ "momoland" ],
+      gen: ["gen3"]  
+      }
+  },
+  {
+    name: "Sihyeon (Everglow)",
+    img: "k58EXKj.png",
+    opts: {
+      group: [ "everglow" ],
+      gen: ["gen4"]  
+      }
+  },
+  {
+    name: "E:U (Everglow)",
+    img: "XTOSt4z.png",
+    opts: {
+      group: [ "everglow" ],
+      gen: ["gen4"]  
+      }
+  },
+  {
+    name: "Mia (Everglow)",
+    img: "zmgP5b5.png",
+    opts: {
+      group: [ "everglow" ],
+      gen: ["gen4"]  
+      }
+  },
+  {
+    name: "Onda (Everglow)",
+    img: "DYg3uDK.png",
+    opts: {
+      group: [ "everglow" ],
+      gen: ["gen4"]  
+      }
+  },
+  {
+    name: "Aisha (Everglow)",
+    img: "qwlyvKS.png",
+    opts: {
+      group: [ "everglow" ],
+      gen: ["gen4"]  
+      }
+  },
+  {
+    name: "Yiren (Everglow)",
+    img: "aDOQVy0.png",
+    opts: {
+      group: [ "everglow" ],
+      gen: ["gen4"]  
+      }
+  },
+  {
+    name: "JiU (Dreamcatcher)",
+    img: "Et2wil2.png",
+    opts: {
+      group: [ "dc" ],
+      gen: ["gen3"]  
+      }
+  },
+  {
+    name: "SuA (Dreamcatcher)",
+    img: "nCavfPD.png",
+    opts: {
+      group: [ "dc" ],
+      gen: ["gen3"]  
+      }
+  },
+  {
+    name: "Siyeon (Dreamcatcher)",
+    img: "hhmk4mC.png",
+    opts: {
+      group: [ "dc" ],
+      gen: ["gen3"]  
+      }
+  },
+  {
+    name: "Handong (Dreamcatcher)",
+    img: "oqNPEEN.png",
+    opts: {
+      group: [ "dc" ],
+      gen: ["gen3"]  
+      }
+  },
+  {
+    name: "Yoohyeon (Dreamcatcher)",
+    img: "eBiS6hm.png",
+    opts: {
+      group: [ "dc" ],
+      gen: ["gen3"]  
+      }
+  },
+  {
+    name: "Dami (Dreamcatcher)",
+    img: "9KpBMSP.png",
+    opts: {
+      group: [ "dc" ],
+      gen: ["gen3"]  
+      }
+  },
+  {
+    name: "Gahyeon (Dreamcatcher)",
+    img: "xCoMkiH.png",
+    opts: {
+      group: [ "dc" ],
+      gen: ["gen3"]  
+      }
+  },
+  {
+    name: "Umji (GFriend)",
+    img: "cAywVfR.png",
+    opts: {
+      group: [ "gfriend" ],
+      gen: ["gen3"]  
+      }
+  }, 
+  {
+    name: "SinB (GFriend)",
+    img: "vXKXdL6.png",
+    opts: {
+      group: [ "gfriend" ],
+      gen: ["gen3"]  
+      }
+  }, 
+  {
+    name: "Yuju (GFriend)",
+    img: "VCOdxIS.png",
+    opts: {
+      group: [ "gfriend" ],
+      gen: ["gen3"]  
+      }
+  }, 
+  {
+    name: "Eunha (GFriend)",
+    img: "8jIvIVw.png",
+    opts: {
+      group: [ "gfriend" ],
+      gen: ["gen3"]  
+      }
+  }, 
+  {
+    name: "Yerin (GFriend)",
+    img: "SsbqUzJ.png",
+    opts: {
+      group: [ "gfriend" ],
+      gen: ["gen3"]  
+      }
+  },
+  {
+    name: "Sowon (GFriend)",
+    img: "VlbDcB8.png",
+    opts: {
+      group: [ "gfriend" ],
+      gen: ["gen3"]  
+      }
+  },
+  {
+    name: "Irene (Red Velvet)",
+    img: "SBvP63c.png",
+    opts: {
+      group: [ "redvelvet" ],
+      gen: ["gen3"]  
+      }
+  },
+  {
+    name: "Joy (Red Velvet)",
+    img: "eBvYzEs.png",
+    opts: {
+      group: [ "redvelvet" ],
+      gen: ["gen3"]  
+      }
+  },
+  {
+    name: "Seulgi (Red Velvet)",
+    img: "frcb38M.png",
+    opts: {
+      group: [ "redvelvet" ],
+      gen: ["gen3"]  
+      }
+  },
+  {
+    name: "Wendy (Red Velvet)",
+    img: "QBbBbh0.png",
+    opts: {
+      group: [ "redvelvet" ],
+      gen: ["gen3"]  
+      }
+  },
+  {
+    name: "Yeri (Red Velvet)",
+    img: "8CsoO5Y.png",
+    opts: {
+      group: [ "redvelvet" ],
+      gen: ["gen3"]  
+      }
+  },
+  {
+  name: "Choerry (Loona)",
+  img: "PlCvX1v.png",
+  opts: {
+    group: [ "loona" ],
+    gen: ["gen4"]  
+    }
+  },
+  {
+  name: "Chuu (Loona)",
+  img: "RKg2uRJ.png",
+  opts: {
+    group: [ "loona" ],
+    gen: ["gen4"]  
+    }
+  },
+  {
+  name: "Go Won (Loona)",
+  img: "4ftWAoi.png",
+  opts: {
+    group: [ "loona" ],
+    gen: ["gen4"]  
+    }
+  },
+  {
+  name: "Haseul (Loona)",
+  img: "GsTtjrt.png",
+  opts: {
+    group: [ "loona" ],
+    gen: ["gen4"]  
+    }
+  },
+  {
+  name: "HeeJin (Loona)",
+  img: "8AHOwOQ.png",
+  opts: {
+    group: [ "loona" ],
+    gen: ["gen4"]  
+    }
+  },
+  {
+  name: "HyunJin (Loona)",
+  img: "2Zv543k.png",
+  opts: {
+    group: [ "loona" ],
+    gen: ["gen4"]  
+    }
+  },
+  {
+  name: "Jinsoul (Loona)",
+  img: "OwKBJ2f.png",
+  opts: {
+    group: [ "loona" ],
+    gen: ["gen4"]  
+    }
+  },
+  {
+  name: "KimLip (Loona)",
+  img: "QGSue9m.png",
+  opts: {
+    group: [ "loona" ],
+    gen: ["gen4"]  
+    }
+  },
+  {
+  name: "Olivia Hye (Loona)",
+  img: "0pc2vka.png",
+  opts: {
+    group: [ "loona" ],
+    gen: ["gen4"]  
+    }
+  },
+  {
+  name: "Vivi (Loona)",
+  img: "CVBIoTf.png",
+  opts: {
+    group: [ "loona" ],
+    gen: ["gen4"]  
+    }
+  },
+  {
+  name: "Yeojin (Loona)",
+  img: "lHC3zir.png",
+  opts: {
+    group: [ "loona" ],
+    gen: ["gen4"]  
+    }
+  },
+  {
+  name: "Yves (Loona)",
+  img: "2cnRrNL.png",
+  opts: {
+    group: [ "loona" ],
+    gen: ["gen4"]  
+    }
+  },
+  {
+  name: "Jieun (Purple Kiss)",
+  img: "XjIihR7.png",
+  opts: {
+    group: [ "purplekiss" ],
+    gen: ["gen4"]  
+    }
+  },
+  {
+  name: "Goeun (Purple Kiss)",
+  img: "WQWqMvp.png",
+  opts: {
+    group: [ "purplekiss" ],
+    gen: ["gen4"]  
+    }
+  },
+  {
+  name: "Dosie (Purple Kiss)",
+  img: "fyKLiW6.png",
+  opts: {
+    group: [ "purplekiss" ],
+    gen: ["gen4"]  
+    }
+  },
+  {
+  name: "Ireh (Purple Kiss)",
+  img: "3qtWcDj.png",
+  opts: {
+    group: [ "purplekiss" ],
+    gen: ["gen4"]  
+    }
+  },
+  {
+  name: "Yuki (Purple Kiss)",
+  img: "YojjYLs.png",
+  opts: {
+    group: [ "purplekiss" ],
+    gen: ["gen4"]  
+    }
+  },
+  {
+  name: "Chaein (Purple Kiss)",
+  img: "jDz9N3R.png",
+  opts: {
+    group: [ "purplekiss" ],
+    gen: ["gen4"]  
+    }
+  },
+  {
+  name: "Swan (Purple Kiss)",
+  img: "0W3AgQ5.png",
+  opts: {
+    group: [ "purplekiss" ],
+    gen: ["gen4"]  
+    }
+  },
+  {
+  name: "Nana (Woo!Ah!)",
+  img: "LMsVgMr.png",
+  opts: {
+    group: [ "wooah" ],
+    gen: ["gen4"]  
+    }
+  },
+  {
+  name: "Wooyeon (Woo!Ah!)",
+  img: "IB0WvNW.png",
+  opts: {
+    group: [ "wooah" ],
+    gen: ["gen4"]  
+    }
+  },
+  {
+  name: "Sora (Woo!Ah!)",
+  img: "ldkYQN6.png",
+  opts: {
+    group: [ "wooah" ],
+    gen: ["gen4"]  
+    }
+  },  
+  {
+  name: "Lucy (Woo!Ah!)",
+  img: "xv7C4cs.png",
+  opts: {
+    group: [ "wooah" ],
+    gen: ["gen4"]  
+    }
+  },
+  {
+  name: "Minseo (Woo!Ah!)",
+  img: "q5fVW0O.png",
+  opts: {
+    group: [ "wooah" ],
+    gen: ["gen4"]  
+    }
+  },
+  {
+  name: "Gaeul (Ive)",
+  img: "HnJ9FtR.png",
+  opts: {
+    group: [ "ive" ],
+    gen: ["gen4"]  
+    }
+  },
+  {
+  name: "Leeseo (Ive)",
+  img: "JGqlJic.png",
+  opts: {
+    group: [ "ive" ],
+    gen: ["gen4"]  
+    }
+  },
+  {
+  name: "Liz (Ive)",
+  img: "9nEg5l0.png",
+  opts: {
+    group: [ "ive" ],
+    gen: ["gen4"]  
+    }
+  },
+  {
+  name: "Rei (Ive)",
+  img: "jsNLm9a.png",
+  opts: {
+    group: [ "ive" ],
+    gen: ["gen4"]  
+    }
+  },
+  {
+  name: "Jihan (Weeekly)",
+  img: "iVbO9Sf.png",
+  opts: {
+    group: [ "weeekly" ],
+    gen: ["gen4"]
+    }
+  },
+  {
+  name: "Zoa (Weeekly)",
+  img: "57xFaAc.png",
+  opts: {
+    group: [ "weeekly" ],
+    gen: ["gen4"]
+    }
+  },
+  {
+  name: "Jaehee (Weeekly)",
+  img: "GVU4Qhm.png",
+  opts: {
+    group: [ "weeekly" ],
+    gen: ["gen4"]
+    }
+  },
+  {
+  name: "Soojin (Weeekly)",
+  img: "wFwPeUs.png",
+  opts: {
+    group: [ "weeekly" ],
+    gen: ["gen4"]
+    }
+  },
+  {
+  name: "Jiyoon (Weeekly)",
+  img: "ZzOV3Ou.png",
+  opts: {
+    group: [ "weeekly" ],
+    gen: ["gen4"]
+    }
+  },
+  {
+  name: "Monday (Weeekly)",
+  img: "jsYTsYR.png",
+  opts: {
+    group: [ "weeekly" ],
+    gen: ["gen4"]
+    }
+  },
+  {
+  name: "Soeun (Weeekly)",
+  img: "PQ4RPLr.png",
+  opts: {
+    group: [ "weeekly" ],
+    gen: ["gen4"]
+    }
+  },
+  {
+  name: "Hyejeong (AOA)",
+  img: "gpg2dBy.png",
+  opts: {
+    group: [ "AOA" ],
+    gen: ["gen3"]  
+    }
+  },
+    {
+  name: "Seolhyun (AOA)",
+  img: "GccsEZ4.png",
+  opts: {
+    group: [ "AOA" ],
+    gen: ["gen3"]  
+    }
+  },
+    {
+  name: "Chanmi (AOA)",
+  img: "eVHkEyJ.png",
+  opts: {
+    group: [ "AOA" ],
+    gen: ["gen3"]  
+    }
+  },
+    {
+  name: "Yuna  (AOA)",
+  img: "hAYxSZk.png",
+  opts: {
+    group: [ "AOA" ],
+    gen: ["gen3"]  
+    }
+  },
+    {
+  name: "Jimin  (AOA)",
+  img: "FXe8cJF.png",
+  opts: {
+    group: [ "AOA" ],
+    gen: ["gen3"]  
+    }
+  },
+      {
+  name: "Choa  (AOA)",
+  img: "kkCyOlA.png",
+  opts: {
+    group: [ "AOA" ],
+    gen: ["gen3"]  
+    }
+  },
+  {
+  name: "Youkyung (AOA)",
+  img: "cvjGZUh.png",
+  opts: {
+    group: [ "AOA" ],
+    gen: ["gen3"]  
+    }
+  },
+  {
+  name: "Eunbi (IZ*ONE)",
+  img: "1Qn1s6t.png",
+  opts: {
+    group: [ "izone" ],
+    gen: ["gen4"]  
+    }
+  },
+  {
+  name: "Sakura (IZ*ONE/LESSERAFIM)",
+  img: "BXrE4gO.png",
+  opts: {
+    group: [ "izone", "lesserafim" ],
+    gen: ["gen4"]  
+    }
+  },
+  {
+  name: "Hyewon (IZ*ONE)",
+  img: "CA6zCP3.png",
+  opts: {
+    group: [ "izone" ],
+    gen: ["gen4"]  
+    }
+  },
+  {
+  name: "Yena (IZ*ONE)",
+  img: "CVTD8A5.png",
+  opts: {
+    group: [ "izone" ],
+    gen: ["gen4"]  
+    }
+  },
+    {
+  name: "Chaeyeon (IZ*ONE)",
+  img: "7ijqfXE.png",
+  opts: {
+    group: [ "izone" ],
+    gen: ["gen4"]  
+    }
+  },
+  {
+  name: "Chaewon (IZ*ONE/LESSERAFIM)",
+  img: "pSeobna.png",
+  opts: {
+    group: [ "izone" ],
+    gen: ["gen4"]  
+    }
+  },
+  {
+  name: "Minju (IZ*ONE)",
+  img: "HmwjndG.png",
+  opts: {
+    group: [ "izone" ],
+    gen: ["gen4"]  
+    }
+  },
+  {
+  name: "Nako (IZ*ONE)",
+  img: "I6HhEM7.png",
+  opts: {
+    group: [ "izone" ],
+    gen: ["gen4"]  
+    }
+  },
+  {
+  name: "Hitomi (IZ*ONE)",
+  img: "A4d2fpD.png",
+  opts: {
+    group: [ "izone" ],
+    gen: ["gen4"]  
+    }
+  },
+  {
+  name: "Yuri (IZ*ONE)",
+  img: "sOIr1xv.png",
+  opts: {
+    group: [ "izone" ],
+    gen: ["gen4"]  
+    }
+  },
+  {
+  name: "Yujin (IZ*ONE/Ive)",
+  img: "3AyDr1A.png",
+  opts: {
+    group: [ "izone", "ive" ],
+    gen: ["gen4"]  
+    }
+  },
+  {
+  name: "Wonyoung (IZ*ONE/Ive)",
+  img: "8lHnWar.png",
+  opts: {
+    group: [ "izone", "ive" ],
+    gen: ["gen4"]  
+    }
+  },
+  {
+  name: "Soyeon ((G)-Idle)",
+  img: "zlT5mqC.png",
+  opts: {
+    group: [ "gidle" ],
+    gen: ["gen4"]  
+    }
+  },
+  {
+  name: "Miyeon ((G)-Idle)",
+  img: "YW9naa9.png",
+  opts: {
+    group: [ "gidle" ],
+    gen: ["gen4"]  
+    }
+  },
+    {
+  name: "Minnie ((G)-Idle)",
+  img: "vuJU0cG.png",
+  opts: {
+    group: [ "gidle" ],
+    gen: ["gen4"]  
+    }
+  },
+  {
+  name: "Yuqi ((G)-Idle)",
+  img: "1bIyWKE.png",
+  opts: {
+    group: [ "gidle" ],
+    gen: ["gen4"]  
+    }
+  },
+  {
+  name: "Shuhua ((G)-Idle)",
+  img: "r4kgZnO.png",
+  opts: {
+    group: [ "gidle" ],
+    gen: ["gen4"]  
+    }
+  },
+  {
+  name: "Soojin ((G)-Idle)",
+  img: "HowTAi5.png",
+  opts: {
+    group: [ "gidle" ],
+    gen: ["gen4"]  
+    }
+  },
+  {
+  name: "Yeji (ITZY)",
+  img: "f6mvweM.png",
+  opts: {
+    group: [ "itzy" ],
+    gen: ["gen4"]  
+    }
+  },
+  {
+  name: "Lia (ITZY)",
+  img: "Igxpu7Z.png",
+  opts: {
+    group: [ "itzy" ],
+    gen: ["gen4"]  
+    }
+  },
+  {
+  name: "Ryujin (ITZY)",
+  img: "wvi49yy.png",
+  opts: {
+    group: [ "itzy" ],
+    gen: ["gen4"]  
+    }
+  },
+  {
+  name: "Chaeryeong (ITZY)",
+  img: "lld0nVE.png",
+  opts: {
+    group: [ "itzy" ],
+    gen: ["gen4"]  
+    }
+  },
+  {
+  name: "Yuna (ITZY)",
+  img: "ZNTSvXG.png",
+  opts: {
+    group: [ "itzy" ],
+    gen: ["gen4"]  
+    }
+  },
+  {
+  name: "Binnie (Oh My Girl)",
+  img: "3cSiMaE.png",
+  opts: {
+    group: [ "omg" ],
+    gen: ["gen3"]  
+    }
+  },
+  {
+  name: "Arin (Oh My Girl)",
+  img: "BLGPqQk.png",
+  opts: {
+    group: [ "omg" ],
+    gen: ["gen3"]  
+    }
+  },
+  {
+  name: "Jiho (Oh My Girl)",
+  img: "1bQzA1m.png",
+  opts: {
+    group: [ "omg" ],
+    gen: ["gen3"]  
+    }
+  },
+    {
+  name: "Hyojung (Oh My Girl)",
+  img: "c9y10pa.png",
+  opts: {
+    group: [ "omg" ],
+    gen: ["gen3"]  
+    }
+  },
+    {
+  name: "Seunghee (Oh My Girl)",
+  img: "LyGDIQ4.png",
+  opts: {
+    group: [ "omg" ],
+    gen: ["gen3"]  
+    }
+  },
+    {
+  name: "Mimi (Oh My Girl)",
+  img: "Vsb2iua.png",
+  opts: {
+    group: [ "omg" ],
+    gen: ["gen3"]  
+    }
+  },
+  {
+  name: "YooA (Oh My Girl)",
+  img: "bRPhM6j.png",
+  opts: {
+    group: [ "omg" ],
+    gen: ["gen3"]  
+    }
+  },
+  {
+  name: "JinE (Oh My Girl)",
+  img: "7ldHJmE.png",
+  opts: {
+    group: [ "omg" ],
+    gen: ["gen3"]  
+    }
+  },
+  {
+  name: "Giselle (Aespa)",
+  img: "b5znPOV.png",
+  opts: {
+    group: [ "aespa" ],
+    gen: ["gen4"]  
+    }
+  },
+  {
+  name: "Karina (Aespa)",
+  img: "fmNmiCQ.png",
+  opts: {
+    group: [ "aespa" ],
+    gen: ["gen4"]  
+    }
+  },
+    {
+  name: "Ningning (Aespa)",
+  img: "FdrmBkL.png",
+  opts: {
+    group: [ "aespa" ],
+    gen: ["gen4"]  
+    }
+  },
+    {
+  name: "Winter (Aespa)",
+  img: "CyUqyOL.png",
+  opts: {
+    group: [ "aespa" ],
+    gen: ["gen4"]  
+    }
+  },
+  {
+  name: "Isa (STAYC)",
+  img: "mjNanrV.png",
+  opts: {
+    group: [ "stayc" ],
+    gen: ["gen4"]  
+    }
+  },
+  {
+  name: "J (STAYC)",
+  img: "SBYZIxv.png",
+  opts: {
+    group: [ "stayc" ],
+    gen: ["gen4"]  
+    }
+  },
+  {
+  name: "Seeun (STAYC)",
+  img: "5eWLFeO.png",
+  opts: {
+    group: [ "stayc" ],
+    gen: ["gen4"]  
+    }
+  },
+  {
+  name: "Sieun (STAYC)",
+  img: "PEMjzxs.png",
+  opts: {
+    group: [ "stayc" ],
+    gen: ["gen4"]  
+    }
+  },
+  {
+  name: "Sumin (STAYC)",
+  img: "5oGBcy8.png",
+  opts: {
+    group: [ "stayc" ],
+    gen: ["gen4"]  
+    }
+  },
+  {
+  name: "Yoon (STAYC)",
+  img: "N0AxDbJ.png",
+  opts: {
+    group: [ "stayc" ],
+    gen: ["gen4"]  
+    }
+  },
+  {
+  name: "Chaeyoung (Fromis_9)",
+  img: "RfIvcNB.png",
+  opts: {
+    group: [ "fromis9" ],
+    gen: ["gen4"]  
+    }
+  },
+  {
+  name: "Gyuri (Fromis_9)",
+  img: "5PFvwOn.png",
+  opts: {
+    group: [ "fromis9" ],
+    gen: ["gen4"]  
+    }
+  },
+  {
+  name: "Hayoung (Fromis_9)",
+  img: "A6GnSkT.png",
+  opts: {
+    group: [ "fromis9" ],
+    gen: ["gen4"]  
+    }
+  },
+  {
+  name: "Jiheon (Fromis_9)",
+  img: "CmKnAxX.png",
+  opts: {
+    group: [ "fromis9" ],
+    gen: ["gen4"]  
+    }
+  },
+  {
+  name: "Jisun (Fromis_9)",
+  img: "S6NBY5u.png",
+  opts: {
+    group: [ "fromis9" ],
+    gen: ["gen4"]  
+    }
+  },
+  {
+  name: "Jiwon (Fromis_9)",
+  img: "4cYKEca.png",
+  opts: {
+    group: [ "fromis9" ],
+    gen: ["gen4"]  
+    }
+  },
+  {
+  name: "Nagyung (Fromis_9)",
+  img: "u2JzUF7.png",
+  opts: {
+    group: [ "fromis9" ],
+    gen: ["gen4"]  
+    }
+  },
+  {
+  name: "Saerom (Fromis_9)",
+  img: "QmSmE3l.png",
+  opts: {
+    group: [ "fromis9" ],
+    gen: ["gen4"]  
+    }
+  },
+  {
+  name: "Seoyeon (Fromis_9)",
+  img: "HH7LwZD.png",
+  opts: {
+    group: [ "fromis9" ],
+    gen: ["gen4"]  
+    }
+  },
+  {
+  name: "Chaeyoung (Twice)",
+  img: "PSE6byu.png",
+  opts: {
+    group: [ "twice" ],
+    gen: ["gen3"]  
+    }
+  },
+    {
+  name: "Dahyun (Twice)",
+  img: "I5J6syW.png",
+  opts: {
+    group: [ "twice" ],
+    gen: ["gen3"]  
+    }
+  },
+    {
+  name: "Jeongyeon (Twice)",
+  img: "7rLRkE8.png",
+  opts: {
+    group: [ "twice" ],
+    gen: ["gen3"]  
+    }
+  },
+    {
+  name: "Jihyo (Twice)",
+  img: "fQrYFZb.png",
+  opts: {
+    group: [ "twice" ],
+    gen: ["gen3"]  
+    }
+  },
+    {
+  name: "Mina (Twice)",
+  img: "UApjuKz.png",
+  opts: {
+    group: [ "twice" ],
+    gen: ["gen3"]  
+    }
+  },
+    {
+  name: "Momo (Twice)",
+  img: "acpPAue.png",
+  opts: {
+    group: [ "twice" ],
+    gen: ["gen3"]  
+    }
+  },
+    {
+  name: "Nayeon (Twice)",
+  img: "o5ghyP5.png",
+  opts: {
+    group: [ "twice" ],
+    gen: ["gen3"]  
+    }
+  },
+    {
+  name: "Sana (Twice)",
+  img: "S46LzQt.png",
+  opts: {
+    group: [ "twice" ],
+    gen: ["gen3"]  
+    }
+  },
+    {
+  name: "Tzuyu (Twice)",
+  img: "7VXdrXv.png",
+  opts: {
+    group: [ "twice" ],
+    gen: ["gen3"]  
+    }
+  },
+  {
+  name: "Jennie (Blackpink)",
+  img: "i7dipOq.png",
+  opts: {
+    group: [ "blackpink" ],
+    gen: ["gen3"]  
+    }
+  },
+  {
+  name: "Jisoo (Blackpink)",
+  img: "TKmtnpU.png",
+  opts: {
+    group: [ "blackpink" ],
+    gen: ["gen3"]  
+    }
+  },
+  {
+  name: "Lisa (Blackpink)",
+  img: "iVGe8A6.png",
+  opts: {
+    group: [ "blackpink" ],
+    gen: ["gen3"]  
+    }
+  },
+ {
+  name: "Rosé (Blackpink)",
+  img: "NfGNbQ4.png",
+  opts: {
+    group: [ "blackpink" ],
+    gen: ["gen3"]  
+    }
+  },
+  {
+    name: "Yunjin (Lesserafim)",
+    img: "sqeJXTT.png",
+    opts: {
+      group: [ "lesserafim" ],
+      gen: ["gen4"]  
+    }
+  },
+  {
+    name: "Kazuha (Lesserafim)",
+    img: "OvPEhuJ.png",
+    opts: {
+      group: [ "lesserafim" ],
+      gen: ["gen4"]  
+      }
+    },
+    {
+      name: "Garam (Lesserafim)",
+      img: "d5Ivt7h.png",
+      opts: {
+        group: [ "lesserafim" ],
+        gen: ["gen4"]  
+        }
+    },
+    {
+      name: "Eunchae (Lesserafim)",
+      img: "JXMaf2R.png",
+      opts: {
+        group: [ "lesserafim" ],
+        gen: ["gen4"]  
+        }
+    },    
+];
